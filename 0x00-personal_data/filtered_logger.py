3f7fd74df3f94e9d17cd5bbc9948d8460287ee8e
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-10 11:30:23 +0200

    formatted log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,29 @@
 """
 filtered_logger:
 """
+import logging
 import re
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        formatted_msg = logging.Formatter(self.FORMAT).format(record)
+        return formatted_msg
 
 
 def filter_datum(fields: List[str], redaction: str,
