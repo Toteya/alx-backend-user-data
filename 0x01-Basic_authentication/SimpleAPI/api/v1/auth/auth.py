897a1786de114826f1690aba5bb7f95f9ec77a1c
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-11 15:33:18 +0200

    taskk 4

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -14,13 +14,19 @@ class Auth:
     def require_auth(self, path: str, excluded_path: List[str]) -> bool:
         """ Require auth
         """
+        if not excluded_path:
+            return True
+        if path is None:
+            return True
+        if path.strip('/') not in [ep.strip('/') for ep in excluded_path]:
+            return True
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """ Authorization header
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current user
         """
