b3709548336ae7ff510942df4d245dc682ad1d6d
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-11 18:33:57 +0200

    task 8: decode b64

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -3,13 +3,15 @@
 basic_auth: Basic Authorization
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
     Basic Authentication class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """ Returns the Base64 part of the Authorization header for a Basic
         Authentication
         """
@@ -20,3 +22,21 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.lstrip('Basic ')
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+    ) -> str:
+        """ Returns the decodes value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_utf_str = decoded_bytes.decode('utf-8')
+        except base64.binascii.Error:
+            return None
+
+        return decoded_utf_str
