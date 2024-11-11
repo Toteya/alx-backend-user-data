d705873930366b17929245993088532ceeae9799
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-11 17:22:18 +0200

    task 6: Basic Auth

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+basic_auth: Basic Authorization
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Authentication class
+    """
+    pass
