041024c2a76932f985bfdb80b5e9f3668c68575d
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-11 17:53:36 +0200

    Task 7: extract basic_auth header

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -9,4 +9,14 @@ class BasicAuth(Auth):
     """
     Basic Authentication class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Returns the Base64 part of the Authorization header for a Basic
+        Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.lstrip('Basic ')
