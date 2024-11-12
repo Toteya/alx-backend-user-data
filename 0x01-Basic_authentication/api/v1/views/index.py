ba6dae1f2c2303913e5c852518e74f426296d9e0
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-12 08:36:57 +0200

    pycodestyle

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,11 +25,13 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized():
     """ GET /api/v1/unauthorized
     """
     return abort(401)
+
 
 @app_views.route('/forbidden', strict_slashes=False)
 def forbidden():
