1a43c7a27cae9ee9233a99bac32c6815a0359cf0
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-13 19:45:40 +0200

    session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,23 @@
 session_auth: Session Authentication
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session authentication class
     """
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session id for a user_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        
+        return session_id
