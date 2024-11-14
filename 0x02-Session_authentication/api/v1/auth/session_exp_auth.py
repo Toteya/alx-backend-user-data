f7457ae6b67c11ff32e18ea42a68b0e56708dc34
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-14 23:31:34 +0200

    session expiration

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+session_exp_auth module: Session Authentication expiration
+"""
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+import os
+
+
+class SessionExpAuth(SessionAuth):
+    """
+    Session Authentication Expiration class
+    """
+    def __init__(self):
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION'))
+        except ValueError:
+            self.session_duration = 0
+        super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session id for a user_id
+        """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dictionary = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if session_id is None:
+            return None
+        session_dictionary = self.user_id_by_session_id.get(session_id)
+        if session_dictionary is None:
+            return None
+        user_id = session_dictionary.get('user_id')
+        if self.session_duration <= 0:
+            return user_id
+        if session_dictionary.get('created_at') is None:
+            return None
+        td_duration = timedelta(seconds=self.session_duration)
+        if session_dictionary.get('created_at') + td_duration < datetime.now():
+            return None
+        return user_id
