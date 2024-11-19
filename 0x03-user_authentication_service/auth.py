8f661aa493be4dd3f5e02bd0fbfa89f833497575
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-19 15:56:04 +0200

    validate login credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,6 +27,16 @@ class Auth:
         hashed_password = _hash_password(password)
         return self._db.add_user(email=email, hashed_password=hashed_password)
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validates the email and password provided by the user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
 
 def _hash_password(password: str) -> bytes:
     """ Converts and returns the given passsword in bytes
