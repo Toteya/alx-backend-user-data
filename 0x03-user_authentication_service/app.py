b197cfe808951855da4e86cf712dc0f8115f71a9
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-19 17:30:25 +0200

    login implentations

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 module app: A Flask web application
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, make_response, request
 
 
 app = Flask(__name__)
@@ -18,16 +18,29 @@ def welcome():
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
-    """Handles POST /users"""
+    """Registers a new users"""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         AUTH.register_user(email, password)
-        output = {"email": "{}".format(email), "message": "user created"}
+        output = {"email": "{}", "message": "user created"}
     except ValueError:
         output = {"message": "email already registered"}
     return jsonify(output)
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Login an existing user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email=email, password=password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": "{}".format(email), "message": "logged in"}))
+    response.set_cookie('session_id', session_id)
+    return response
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
