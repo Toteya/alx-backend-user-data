eb3ab2c86a89bb4a05ccd17922da4d5bcf0218fc
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-19 15:14:46 +0200

    POST user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,29 @@
 """
 module app: A Flask web application
 """
+from auth import Auth
 from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def welcome():
+    """Welcome message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users(email: str, password: str):
+    """Handles POST /users"""
+    try:
+        AUTH.register_user(email=email, password=password)
+        output = {"email": "{}", "message": "user created".format(email)}
+    except ValueError:
+        output = {"message": "email already registered"}
+    return jsonify(output)
 
 
 if __name__ == '__main__':
