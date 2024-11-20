086adcafa1cd1b773cdb982cd4b2056eb17aa0b6
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-20 18:10:36 +0200

    correction: _generate_uuid is module level function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,11 +38,6 @@ class Auth:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
 
-    def _generate_uuid(self) -> str:
-        """ Generates and returns a UUID
-        """
-        return str(uuid.uuid4())
-
     def create_session(self, email: str) -> str:
         """ Creates a new session_id for the given user
         """
@@ -50,7 +45,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return None
-        session_id = self._generate_uuid()
+        session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
@@ -76,7 +71,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-        token = self._generate_uuid()
+        token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
 
@@ -100,3 +95,9 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password_bytes, salt)
 
     return hashed_password
+
+
+def _generate_uuid() -> str:
+    """ Generates and returns a UUID
+    """
+    return str(uuid.uuid4())
