0c089badb26317a1b891c48e37d07caf523ebcef
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-20 13:39:56 +0200

    kwargs type hint: Dict

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Dict
 from user import Base, User
 
 
@@ -47,7 +48,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: str, **kwargs) -> None:
+    def update_user(self, user_id: str, **kwargs: Dict) -> None:
         """ Updates a user based on the given keyword arguments
         """
         user = self.find_user_by(id=user_id)
