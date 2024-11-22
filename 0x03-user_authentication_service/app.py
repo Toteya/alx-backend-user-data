e4525e2d1b329d5847c11030d211023c6f038475
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-22 08:17:51 +0200

    try

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,8 +61,8 @@ def profile():
     """ Returns a user profile if the given session_id exists
     """
     session_id = request.cookies.get('session_id')
-    if session_id is None:
-        abort(403)
+    # if session_id is None:
+    #     abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
